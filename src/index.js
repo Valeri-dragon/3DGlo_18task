43cb1a5e27640ebeb91f79e9c6e8f88774742e7d
Valeriya Terebilova <yakontekst01@yandex.ru>
2023-02-22 17:48:20 +0300

    start task 20

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import timer from './modules/timer'
+import menu from './modules/menu'
+import modal from './modules/modal'
 
 timer("23 febrary 2023");
+menu();
+modal()
--- src/modules/two.js
+++
@@ -1,5 +0,0 @@
-const moduleTwo = () => {
-  const two = "two module";
-  console.log(two);
-};
-export default moduleTwo;
