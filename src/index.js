4e10202d51c059b440c54cfa7284476f56c0e483
Valeriya Terebilova <yakontekst01@yandex.ru>
2023-02-25 15:11:07 +0300

    modified

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import timer from './modules/timer'
-import menu from './modules/menu'
-import modal from './modules/modal'
-import calculator from './modules/calculator';
+import timer from "./modules/timer";
+import menu from "./modules/menu";
+import modal from "./modules/modal";
+import calculator from "./modules/calculator";
+import forms from "./modules/forms";
 
 timer("23 febrary 2023");
 menu();
-modal()
-calculator()
+modal();
+calculator();
+forms();
---
+++ src/modules/forms.js
@@ -0,0 +1,74 @@
+const forms = () => {
+  const l = (lo) => {
+    console.log(lo);
+  };
+
+  //name="user_name" //type="text"
+  //name="user_email"// type="email"
+  //name="user_phone" //type="tel"
+  //name="user_message" //.mess
+
+  const nameInputs = document.querySelectorAll('input[type="text"]');
+  const emailInputs = document.querySelectorAll('input[type="email"]');
+  const userMessageInput = document.querySelector(".mess");
+  let str;
+  let span = document.createElement("span");
+
+  const removeError = () => {
+    span.remove();
+  };
+
+  const error = (e, str) => {
+    span.style.display = "block";
+    span.classList.add("error");
+    span.style.color = "red";
+    span.style.position = `absolute`;
+    span.style.bottom = `89px`;
+    span.style.right = `77px`;
+        span.style.padding = `5px`;
+    span.style.background = `#fff`;
+    span.textContent = str;
+    e.before(span);
+  };
+
+  const validateCyrillic = (e) => {
+    e.addEventListener("input", (ev) => {
+      removeError();
+
+      if (!/[а-я-\s]+/gi.test(ev.target.value)) {
+        str = "Вводите кириллицу, пробел и дефис";
+        error(e, str);
+      } else {
+        removeError();
+        ev.target.value;
+      }
+    });
+  };
+
+  const validateEmail = (e) => {
+    e.addEventListener("input", (ev) => {
+      if (/(([\-\.\w]+)(@)([\w]+\.)+([\w]{2,4}))/gi.test(ev.target.value)) {
+        removeError();
+        ev.target.value;
+      } else {
+        error(e, str);
+        str = "ведите верные значения";
+      }
+    });
+  };
+
+  nameInputs.forEach((item) => {
+    if (item.hasAttribute("name")) {
+      validateCyrillic(item);
+    }
+  });
+
+  emailInputs.forEach((item) => {
+    validateEmail(item);
+  });
+
+  userMessageInput.addEventListener("input",()=>{
+validateCyrillic(userMessageInput);
+  } );
+};
+export default forms;
