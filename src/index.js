d82254b0e702679e23cbe4e7ec853750cafb71b7
Valeriya Terebilova <yakontekst01@yandex.ru>
2023-02-25 12:03:52 +0300

    start task 21

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import timer from './modules/timer'
 import menu from './modules/menu'
 import modal from './modules/modal'
+import calculator from './modules/calculator';
 
 timer("23 febrary 2023");
 menu();
 modal()
+calculator()
