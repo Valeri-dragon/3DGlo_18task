9602b6bacffcfdb7907f936692a1a43b80cf091f
Valeriya Terebilova <yakontekst01@yandex.ru>
2023-02-28 15:08:38 +0300

    modified menu.js, style.css

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,13 +1,18 @@
 const menu = () => {
   const body = document.querySelector("body");
   const menu = document.querySelector("menu");
-
+  let ID;
   const toggleMenu = () => {
     menu.classList.toggle("active-menu");
   };
   const scroll = (e) => {
     e.preventDefault();
-    const ID = e.target.getAttribute("href").substr(1);
+    if (e.target.hasAttributes("href") && e.target.tagName === "A") {
+      ID = e.target.getAttribute("href").substr(1);
+    } else {
+      ID = e.target.parentNode.getAttribute("href").substr(1);
+    }
+
     document
       .getElementById(ID)
       .scrollIntoView({ behavior: "smooth", block: "start" });
@@ -15,11 +20,11 @@ const menu = () => {
   body.addEventListener("click", (e) => {
     if (e.target.matches(".close-btn") || e.target.closest(".menu")) {
       toggleMenu();
-    } else if (e.target.id === "link_service" || e.target.closest("ul>li>a")) {
+    } else if (e.target.closest("#link_service")) {
       scroll(e);
-      if (e.target.closest("ul>li>a")) {
-        toggleMenu();
-      }
+    } else if (e.target.closest("ul>li>a")) {
+      scroll(e);
+      toggleMenu();
     }
   });
 };
